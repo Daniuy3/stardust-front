657e1032d730633cc157eacdadd23ead6c906bda
Daniel Trinidad <daniuy524@gmail.com>
2026-06-02 11:21:00 -0600

    feat(publict redirect url env updated):

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -6,7 +6,7 @@ import { FaGoogle } from 'react-icons/fa'
 export const GoogleLogin = () => {
 
     const handleLoginWithGoogle = () => {
-        window.location.href = `${process.env.API_URL}/auth/oauth/google/redirect`;
+        window.location.href = `${process.env.PUBLIC_REDIRECT_URL}/auth/oauth/google/redirect`;
     }
     
   return (
